dd46d4cdb0ac99474132801c08a1b7ee1a9b6d3f
Jhonny Marcelo <jhonny-marcelo@outlook.com>
2025-07-28 19:04:27 -0300

    Rendering Array Of Objects

--- src/components/Fruits.jsx
+++ src/components/Fruits.jsx
@@ -1,10 +1,18 @@
 export default function Fruits() {
-    const fruits = ["Apple", "Banana", "Cherry"];
+    // const fruits = ["Apple", "Banana", "Cherry"];
+    const fruits = [
+        { name: "Apple", price: 10, emoji: "🍎" },
+        { name: "Banana", price: 5, emoji: "🍌" },
+        { name: "Cherry", price: 15, emoji: "🍒" }      
+    ];
+
     return (
         <div>
             <ul>
             {fruits.map((fruit) => (
-                <li key={fruit}>{fruit}</li>
+                <li key={fruit.name}>
+                    {fruit.emoji} {fruit.name} - ${fruit.price}
+                </li>
             ))}
             </ul>
         </div>
